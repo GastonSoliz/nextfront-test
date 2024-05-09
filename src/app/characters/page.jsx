b8a95097c261f9aae5056c27243ed6cffe991938
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-05-09 19:07:13 -0300

    add(-): creating fetch and adding CardFilm component

--- src/app/characters/page.jsx
+++ src/app/characters/page.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function CharactersPage() {
   return (
     <>
+      <Link href="/films">
+        <p>FILMS</p>
+      </Link>
       <h1>PAGINA PERSONAJES</h1>
     </>
   );
