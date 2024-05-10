21951489b06db111952db017654e7a77f33e15be
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-05-09 22:24:49 -0300

    add(-): create api fetch and import CharacterCard component

--- src/app/films/[id]/page.jsx
+++ src/app/films/[id]/page.jsx
@@ -1,7 +1,26 @@
-export default function Film() {
+import FilmCharacter from "@/components/CardFilmCharacter/FilmCharacterCard";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Film({ params }) {
+  const staticData = await fetch(`https://swapi.dev/api/films/${params.id}`);
+  const infoFilm = await staticData.json();
+
   return (
     <>
-      <h1>PAGINA FILM INDIVIDUAL</h1>
+      <Link href={`/films`}>Films</Link>
+      <h1>{infoFilm.title}</h1>
+      <Image
+        src="https://static.wikia.nocookie.net/starwars/images/c/cc/Star-wars-logo-new-tall.jpg/revision/latest?cb=20190313021755"
+        alt="Generic Image"
+        width={300}
+        height={300}
+      />
+      <p>{infoFilm.episode_id}</p>
+      <p>{infoFilm.director}.</p>
+      {infoFilm.characters.map((ch) => (
+        <FilmCharacter url={ch} />
+      ))}
     </>
   );
 }
