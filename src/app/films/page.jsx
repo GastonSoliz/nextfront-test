545619268390231d02c294b746eb704e7b8749d9
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-05-09 22:26:19 -0300

    refactor(-): delete comments and adding CardFilm component

--- src/app/films/page.jsx
+++ src/app/films/page.jsx
@@ -1,28 +1,15 @@
-import CardFilm from "@/components/CardFilm";
+import CardFilm from "@/components/CardFilm/FilmCard";
 import Link from "next/link";
 import React from "react";
-
-// export async function getStaticProps() {
-//   const data = await fetch("https://swapi.dev/api/films");
-//   const films = await data.json();
-
-//   console.log(films);
-
-//   return {
-//     props: {},
-//   };
-// }
 
 export default async function FilmsPage() {
   const staticData = await fetch("https://swapi.dev/api/films");
   const films = await staticData.json();
 
-  //console.log(films.results);
-
   return (
     <>
       <Link href="/characters">
-        <p>FILMS</p>
+        <p>Personajes</p>
       </Link>
       <h1>Página de Películas</h1>
 
