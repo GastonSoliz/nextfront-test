ce33dfa5b8110fb3eac8b038172f8bb706d73bc7
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-05-09 22:23:51 -0300

    add(-): create api fetch and import CharacterCard component

--- src/app/characters/page.jsx
+++ src/app/characters/page.jsx
@@ -1,13 +1,22 @@
+import CharacterCard from "@/components/CardCharacter/CharacterCard";
 import Link from "next/link";
 import React from "react";
 
-export default function CharactersPage() {
+export default async function CharactersPage() {
+  const staticData = await fetch("https://swapi.dev/api/people");
+  const characters = await staticData.json();
+
+  //console.log(characters.results);
+
   return (
     <>
       <Link href="/films">
         <p>FILMS</p>
       </Link>
       <h1>PAGINA PERSONAJES</h1>
+      {characters.results.map((ch) => (
+        <CharacterCard ch={ch} />
+      ))}
     </>
   );
 }
